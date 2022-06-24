6e397848e724e048f34b1c0388ab52ef26d2891c
Leonardo Sianturi <leonardo.robinsar@gmail.com>
2022-06-24 20:59:59 +0700

    Updates

---
+++ precache-manifest.a1ade2387e0b79c88cafc21b278bccdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b600bf7b6a30de0fdf06",
+    "url": "/todolist-react/static/css/main.c4325df8.chunk.css"
+  },
+  {
+    "revision": "b600bf7b6a30de0fdf06",
+    "url": "/todolist-react/static/js/main.b600bf7b.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/todolist-react/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "d2f71458963558dcbd79",
+    "url": "/todolist-react/static/js/2.d2f71458.chunk.js"
+  },
+  {
+    "revision": "48c48317536c057e2732",
+    "url": "/todolist-react/static/js/runtime~main.48c48317.js"
+  },
+  {
+    "revision": "5c1710de646bf18a8f4e322cf5b572d4",
+    "url": "/todolist-react/index.html"
+  }
+];
